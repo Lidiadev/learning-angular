4ab2378e17480d970b1b766e592a680fb695908e
necula_lidia <necula_lidia@yahoo.com>
2020-06-19 18:33:48 +0200

    Add router links

--- src/products/product-detail.component.ts
+++ src/products/product-detail.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
-import { ProductService } from './product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,9 +9,27 @@ import { ProductService } from './product.service';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
-  product: IProduct ;
+  product: IProduct;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute,
+              private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+      let id = +this.route.snapshot.paramMap.get('id');
+      this.pageTitle = `: ${id}`;
+      this.product = {
+        "productId": id,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2019",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "assets/images/leaf_rake.png"
+      };
+  }
+
+  onBack(): void{
+      this.router.navigate(['/products']);
+  }
 }
