040ed01538e3b11e5360595a02d34b31c4a451e2
necula_lidia <necula_lidia@yahoo.com>
2020-06-19 17:35:31 +0200

    Add routing

---
+++ src/products/product-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle = 'Product Detail';
+  product: IProduct ;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
